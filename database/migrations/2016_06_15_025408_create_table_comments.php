27a9d15b817f1f75b20d2d81b7909faed58415f0
munishguptamg <munishguptamg@gmail.com>
2016-06-15 11:22:44 +0530

    Adding Migrations
    Adding Migrations

---
+++ database/migrations/2016_06_15_025408_create_table_comments.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableComments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');            
+            $table->text('comments');
+            $table->integer('user_id')->unsigned();
+            $table->integer('hotel_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('hotel_id')->references('id')->on('hotels')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
